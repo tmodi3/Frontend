f83d5a9652142f8ff15d2b3f9bd8d9a136511b03
tmodi3 <114708024+tmodi3@users.noreply.github.com>
2023-10-01 20:01:01 -0500

    Final commit

--- App.js
+++ App.js
@@ -1,40 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
-
-import React, 
-{ useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import './App.css';  // Make sure this file exists or create it
 
 function App() {
-const [selectedBreed, setSelectedBreed] = useState('');
+  const [breeds, setBreeds] = useState([]);
+  const [selectedBreed, setSelectedBreed] = useState('');
+  const [selectedSubBreed, setSelectedSubBreed] = useState('');
   const [images, setImages] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [favorites, setFavorites] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    if (selectedBreed) {
-      fetch(`https://dog.ceo/api/breed/${selectedBreed}/images/random/4`)
-        .then(response => response.json())
-        .then(data => {
-          setImages(data.message);
-        });
+    fetch('https://dog.ceo/api/breeds/list/all')
+      .then((response) => response.json())
+      .then((data) => {
+        setBreeds(Object.keys(data.message));
+      });
+  }, []);
+
+  const fetchImages = () => {
+    setIsLoading(true);
+    let url = `https://dog.ceo/api/breed/${selectedBreed}/images/random/3`;
+    if (selectedSubBreed) {
+      url = `https://dog.ceo/api/breed/${selectedBreed}/${selectedSubBreed}/images/random/3`;
     }
-  }, [selectedBreed]);
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setImages(data.message);
+        setIsLoading(false);
+      });
+  };
+
+  const handleBreedSelect = (e) => {
+    setSelectedBreed(e.target.value);
+  };
+
+  const handleSubBreedSelect = (e) => {
+    setSelectedSubBreed(e.target.value);
+  };
+
+  const nextImage = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const prevImage = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+  };
+
+  const addToFavorites = (breed) => {
+    setFavorites([...favorites, breed]);
+  };
 
   return (
     <div className="App">
-      <select onChange={(e) => setSelectedBreed(e.target.value)}>
-        <option>Select a breed</option>
-        {breeds.map((breed, index) => (
-          <option key={index} value={breed}>
-            {breed}
-          </option>
+      <h1>Dog Breeds</h1>
+      <select onChange={handleBreedSelect}>
+        {breeds.map((breed) => (
+          <option key={breed}>{breed}</option>
         ))}
       </select>
-      <div className="gallery">
-        {images.map((image, index) => (
-          <img key={index} src={image} alt={selectedBreed} />
-        ))}
+      <select onChange={handleSubBreedSelect}>
+        {/* Populate this with sub-breeds if applicable */}
+      </select>
+      <button onClick={fetchImages}>Fetch Images</button>
+      <button onClick={() => addToFavorites(selectedBreed)}>Add to Favorites</button>
+      {isLoading && <div className="spinner">Loading...</div>}
+      <div className="carousel">
+        <button onClick={prevImage}>Prev</button>
+        {images.length > 0 && <img src={images[currentIndex]} alt="dog" />}
+        <button onClick={nextImage}>Next</button>
       </div>
+      <h2>Favorites</h2>
+      <ul>
+        {favorites.map((fav, index) => (
+          <li key={index}>{fav}</li>
+        ))}
+      </ul>
     </div>
   );
 }
 
 export default App;
+
--- README.md
+++ README.md
@@ -1,8 +1,1 @@
 # Frontend
-# Dog Gallery App
-
-## Run Locally
-
-1. Clone the repository
-2. Run `npm install`
-3. Run `npm start`
